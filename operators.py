7dcb72937b7220deaa55cada1ac785ccdbcadecc
Sshiril <113382540+Sshiril@users.noreply.github.com>
2023-09-20 15:12:17 -0400

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,42 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def calculator():
+...     a=5
+...     b=2
+...     print(a//b)
+...     print(a/b)
+...     print(a%b)
+...     print(a**b)
+...     print(a*b)
+...     print(a+b)
+...     print(a-b)
+... 
+...     
+calculator()
+2
+2.5
+1
+25
+10
+7
+3
+def calculator():
+    a=5.0
+    b=2.0
+    print(a//b)
+    print(a/b)
+    print(a%b)
+    print(a**b)
+    print(a*b)
+    print(a+b)
+    print(a-b)
+
+    
+calculator()
+2.0
+2.5
+1.0
+25.0
+10.0
+7.0
+3.0
