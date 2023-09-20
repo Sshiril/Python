e6b79e18867d51cf0a9279289aebef3190e6b701
Sshiril <113382540+Sshiril@users.noreply.github.com>
2023-09-20 15:09:57 -0400

    Create eval.py

---
+++ eval.py
@@ -0,0 +1,16 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> ans= eval(input ("enter an expression:    "))
+enter an expression:    5*7/2
+>>> print(ans)
+17.5
+>>> ans= eval(input ("enter an expression:    "))
+enter an expression:    'c'
+>>> print(ans)
+c
+>>> ans= eval(input ("enter an expression:    "))
+enter an expression:    "computer"
+>>> print
+<built-in function print>
+>>> print(ans)
+computer
